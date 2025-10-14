dcdc3f73e3afa501c3475422418beaf97a1ad714
migueel26 <albaruiz.miguel@gmail.com>
2025-10-14 22:54:20 +0200

    Singing Sproutling now faces the SingingCropBlock

--- src/main/java/net/migueel26/faunaandorchestra/block/custom/SingingCropBlock.java
+++ src/main/java/net/migueel26/faunaandorchestra/block/custom/SingingCropBlock.java
@@ -1,40 +1,51 @@
 package net.migueel26.faunaandorchestra.block.custom;
 
+import com.mojang.serialization.MapCodec;
 import net.migueel26.faunaandorchestra.block.entity.SingingCropBlockEntity;
 import net.migueel26.faunaandorchestra.entity.ModEntities;
+import net.migueel26.faunaandorchestra.entity.custom.SproutlingEntity;
 import net.migueel26.faunaandorchestra.item.ModItems;
 import net.migueel26.faunaandorchestra.particles.ModParticleTypes;
 import net.migueel26.faunaandorchestra.sound.ModSounds;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.monster.Ravager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class SingingCropBlock extends CropBlock implements EntityBlock {
+public class SingingCropBlock extends BushBlock implements EntityBlock, BonemealableBlock {
+    public static final MapCodec<SingingCropBlock> CODEC = simpleCodec(SingingCropBlock::new);
     public static int MAX_AGE = 3;
     public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 3);
     public static final BooleanProperty FINAL = BooleanProperty.create("final");
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     private static final VoxelShape[] SHAPE_BY_AGE =
             new VoxelShape[]{
                     Block.box(0.0, 0.0, 0.0, 16.0, 2.0, 16.0),
@@ -48,6 +59,59 @@ public class SingingCropBlock extends CropBlock implements EntityBlock {
         this.registerDefaultState(this.getStateDefinition().any()
                 .setValue(AGE, 0)
                 .setValue(FINAL, false));
+    }
+
+    public int getAge(BlockState state) {
+        return state.getValue(this.getAgeProperty());
+    }
+
+    public final boolean isMaxAge(BlockState state) {
+        return this.getAge(state) >= this.getMaxAge();
+    }
+
+    @Override
+    protected boolean isRandomlyTicking(BlockState state) {
+        return !this.isMaxAge(state);
+    }
+
+    @Override
+    protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (!level.isAreaLoaded(pos, 1)) return; // Forge: prevent loading unloaded chunks when checking neighbor's light
+        if (level.getRawBrightness(pos, 0) >= 9) {
+            int i = this.getAge(state);
+            if (i < this.getMaxAge()) {
+                float f = getGrowthSpeed(state, level, pos);
+                if (net.neoforged.neoforge.common.CommonHooks.canCropGrow(level, pos, state, random.nextInt((int)(25.0F / f) + 1) == 0)) {
+                    level.setBlock(pos, this.getStateForAge(i + 1, state.getValue(FACING)), 2);
+                    net.neoforged.neoforge.common.CommonHooks.fireCropGrowPost(level, pos, state);
+                }
+            }
+        }
+    }
+
+    public void growCrops(Level level, BlockPos pos, BlockState state) {
+        int i = this.getAge(state) + this.getBonemealAgeIncrease(level);
+        int j = this.getMaxAge();
+        if (i > j) {
+            i = j;
+        }
+
+        level.setBlock(pos, this.getStateForAge(i, state.getValue(FACING)), 2);
+    }
+
+    protected int getBonemealAgeIncrease(Level level) {
+        return Mth.nextInt(level.random, 2, 5);
+    }
+
+    public BlockState getStateForAge(int age, Direction direction) {
+        return this.defaultBlockState().setValue(this.getAgeProperty(), Integer.valueOf(age))
+                .setValue(FACING, direction);
+    }
+
+
+    @Override
+    protected boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos) {
+        return state.getBlock() instanceof net.minecraft.world.level.block.FarmBlock;
     }
 
     @Override
@@ -81,31 +145,162 @@ public class SingingCropBlock extends CropBlock implements EntityBlock {
             level.scheduleTick(pos, this, 20);
             level.setBlock(pos, state.setValue(FINAL, true), 3);
         } else {
-            ModEntities.SINGING_SPROUTLING.get().spawn(level, pos, MobSpawnType.NATURAL);
+            SproutlingEntity entity = new SproutlingEntity(ModEntities.SINGING_SPROUTLING.get(), level);
+
+            entity.moveTo(pos, 0f, 0f);
+            entity.setYHeadRot(getYRot(state.getValue(FACING)));
+            entity.setYBodyRot(entity.getYRot());
+            level.addFreshEntity(entity);
+
             level.destroyBlock(pos, false);
             super.tick(state, level, pos, random);
         }
 
     }
 
+    private float getYRot(Direction facing) {
+        return switch (facing) {
+            case NORTH -> 180f;
+            case SOUTH -> 0f;
+            case WEST  -> 90f;
+            case EAST  -> -90f;
+            default -> 0f;
+        };
+    }
+
+    protected static float getGrowthSpeed(BlockState blockState, BlockGetter p_52274_, BlockPos p_52275_) {
+        Block p_52273_ = blockState.getBlock();
+        float f = 1.0F;
+        BlockPos blockpos = p_52275_.below();
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                float f1 = 0.0F;
+                BlockState blockstate = p_52274_.getBlockState(blockpos.offset(i, 0, j));
+                net.neoforged.neoforge.common.util.TriState soilDecision = blockstate.canSustainPlant(p_52274_, blockpos.offset(i, 0, j), net.minecraft.core.Direction.UP, blockState);
+                if (soilDecision.isDefault() ? blockstate.getBlock() instanceof net.minecraft.world.level.block.FarmBlock : soilDecision.isTrue()) {
+                    f1 = 1.0F;
+                    if (blockstate.isFertile(p_52274_, p_52275_.offset(i, 0, j))) {
+                        f1 = 3.0F;
+                    }
+                }
+
+                if (i != 0 || j != 0) {
+                    f1 /= 4.0F;
+                }
+
+                f += f1;
+            }
+        }
+
+        BlockPos blockpos1 = p_52275_.north();
+        BlockPos blockpos2 = p_52275_.south();
+        BlockPos blockpos3 = p_52275_.west();
+        BlockPos blockpos4 = p_52275_.east();
+        boolean flag = p_52274_.getBlockState(blockpos3).is(p_52273_) || p_52274_.getBlockState(blockpos4).is(p_52273_);
+        boolean flag1 = p_52274_.getBlockState(blockpos1).is(p_52273_) || p_52274_.getBlockState(blockpos2).is(p_52273_);
+        if (flag && flag1) {
+            f /= 2.0F;
+        } else {
+            boolean flag2 = p_52274_.getBlockState(blockpos3.north()).is(p_52273_)
+                    || p_52274_.getBlockState(blockpos4.north()).is(p_52273_)
+                    || p_52274_.getBlockState(blockpos4.south()).is(p_52273_)
+                    || p_52274_.getBlockState(blockpos3.south()).is(p_52273_);
+            if (flag2) {
+                f /= 2.0F;
+            }
+        }
+
+        return f;
+    }
+
+    @Nullable
     @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        Direction direction = context.getHorizontalDirection();
+        BlockPos blockpos = context.getClickedPos();
+        BlockPos blockpos1 = blockpos.relative(direction);
+        Level level = context.getLevel();
+        return level.getBlockState(blockpos1).canBeReplaced(context) && level.getWorldBorder().isWithinBounds(blockpos1)
+                ? this.defaultBlockState().setValue(FACING, direction.getOpposite())
+                : null;
+    }
+    @Override
+    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        net.neoforged.neoforge.common.util.TriState soilDecision = level.getBlockState(pos.below()).canSustainPlant(level, pos.below(), net.minecraft.core.Direction.UP, state);
+        if (!soilDecision.isDefault()) return soilDecision.isTrue();
+        return hasSufficientLight(level, pos) && super.canSurvive(state, level, pos);
+    }
+
+    public static boolean hasSufficientLight(LevelReader level, BlockPos pos) {
+        return level.getRawBrightness(pos, 0) >= 8;
+    }
+
+    @Override
+    protected void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+        if (entity instanceof Ravager && net.neoforged.neoforge.event.EventHooks.canEntityGrief(level, entity)) {
+            level.destroyBlock(pos, true, entity);
+        }
+
+        super.entityInside(state, level, pos, entity);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(LevelReader level, BlockPos pos, BlockState state) {
+        return new ItemStack(this.getBaseSeedId());
+    }
+
+    @Override
+    public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state) {
+        return !this.isMaxAge(state);
+    }
+
+    @Override
+    public boolean isBonemealSuccess(Level level, RandomSource random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
+        this.growCrops(level, pos, state);
+    }
+
+    @Override
+    public MapCodec<? extends BushBlock> codec() {
+        return CODEC;
+    }
+
+    /**
+     * Returns the blockstate with the given rotation from the passed blockstate. If inapplicable, returns the passed blockstate.
+     */
+    @Override
+    protected @NotNull BlockState rotate(BlockState state, Rotation rot) {
+        return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+    }
+
+    /**
+     * Returns the blockstate with the given mirror of the passed blockstate. If inapplicable, returns the passed blockstate.
+     */
+    @Override
+    protected @NotNull BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
     protected ItemLike getBaseSeedId() {
         return ModItems.SINGING_SEED;
     }
 
-    @Override
     public IntegerProperty getAgeProperty() {
         return AGE;
     }
 
-    @Override
     public int getMaxAge() {
         return MAX_AGE;
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(AGE, FINAL);
+        builder.add(AGE, FINAL, FACING);
     }
 
     @Nullable
