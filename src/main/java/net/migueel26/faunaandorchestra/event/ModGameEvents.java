c848c75f43ce0529c3a84657a4be50f7e15c6bf4
migueel26 <albaruiz.miguel@gmail.com>
2025-11-09 19:55:31 +0100

    Frogs are more likely to sing

--- src/main/java/net/migueel26/faunaandorchestra/event/ModGameEvents.java
+++ src/main/java/net/migueel26/faunaandorchestra/event/ModGameEvents.java
@@ -54,7 +54,7 @@ public class ModGameEvents {
     @SubscribeEvent
     public static void quirkyFrogChoir(EntityTickEvent.Post event) {
         if (!event.getEntity().level().isClientSide() &&
-                event.getEntity().tickCount % 80 == 0 &&
+                event.getEntity().tickCount % 60 == 0 &&
                 event.getEntity().level().getRandom().nextFloat() <= 0.01F &&
                 event.getEntity() instanceof QuirkyFrogEntity quirkyFrog
                 && quirkyFrog.isAptForChoir()) {
