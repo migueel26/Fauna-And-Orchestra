15a68c996bb4f7ba47bcd003159e7dfb36e6ece4
migueel26 <albaruiz.miguel@gmail.com>
2026-06-17 23:09:53 +0200

    feat: Added Server test to the CI/CD workflow

--- src/main/java/net/migueel26/faunaandorchestra/FaunaAndOrchestra.java
+++ src/main/java/net/migueel26/faunaandorchestra/FaunaAndOrchestra.java
@@ -59,7 +59,7 @@ import net.neoforged.neoforge.event.server.ServerStartingEvent;
 @Mod(FaunaAndOrchestra.MOD_ID)
 public class FaunaAndOrchestra {
     public static final String MOD_ID = "faunaandorchestra";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
---
+++ src/main/java/net/migueel26/faunaandorchestra/event/CITestHandler.java
@@ -0,0 +1,19 @@
+package net.migueel26.faunaandorchestra.event;
+
+import net.migueel26.faunaandorchestra.FaunaAndOrchestra;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
+
+@EventBusSubscriber(modid = FaunaAndOrchestra.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
+public class CITestHandler {
+
+    @SubscribeEvent
+    public static void onServerStarted(ServerStartedEvent event) {
+        if (System.getenv("CI") != null) {
+            FaunaAndOrchestra.LOGGER.info("[Fauna & Orchestra CI] The server was started successfully. Shutting down server after CI test.");
+
+            event.getServer().halt(false);
+        }
+    }
+}
