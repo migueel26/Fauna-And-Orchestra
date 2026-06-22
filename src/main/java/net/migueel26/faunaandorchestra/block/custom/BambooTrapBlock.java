e2a3f0738b3dd895fdba852ee56d9e30a973a4f9
migueel26 <albaruiz.miguel@gmail.com>
2026-06-22 17:48:16 +0200

    Backported BambooTrapBlock and BE

--- src/main/java/net/migueel26/faunaandorchestra/block/custom/BambooTrapBlock.java
+++ src/main/java/net/migueel26/faunaandorchestra/block/custom/BambooTrapBlock.java
@@ -37,7 +37,6 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class BambooTrapBlock extends HorizontalDirectionalBlock implements EntityBlock {
-    public static final MapCodec<BambooTrapBlock> CODEC = simpleCodec(BambooTrapBlock::new);
     public static final BooleanProperty OPEN = BlockStateProperties.OPEN;
     private static final VoxelShape SHAPE = Block.box(2, 0, 2, 14, 2, 14);
     public BambooTrapBlock(Properties properties) {
@@ -47,15 +46,15 @@ public class BambooTrapBlock extends HorizontalDirectionalBlock implements Entit
     }
 
     @Override
-    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE;
     }
 
     @Override
-    protected void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
         if (entity instanceof LivingEntity livingEntity && level.getBlockEntity(pos) instanceof BambooTrapBlockEntity bambooTrap) {
             if (state.getValue(OPEN)) {
-                Vec3 center = pos.getBottomCenter();
+                Vec3 center = Vec3.atBottomCenterOf(pos);
 
                 bambooTrap.trap();
                 level.setBlock(pos, state.setValue(OPEN, false), 3);
@@ -82,10 +81,10 @@ public class BambooTrapBlock extends HorizontalDirectionalBlock implements Entit
     }
 
     @Override
-    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
         if (!state.getValue(OPEN)) {
             List<LivingEntity> entitiesInside = level.getEntitiesOfClass(LivingEntity.class,
-                    AABB.ofSize(pos.getBottomCenter(), 0.75f, 0.75f, 0.75f));
+                    AABB.ofSize(Vec3.atBottomCenterOf(pos), 0.75f, 0.75f, 0.75f));
 
             if (entitiesInside.isEmpty()) {
                 if (level.getBlockEntity(pos) instanceof BambooTrapBlockEntity bambooTrap) {
@@ -102,7 +101,7 @@ public class BambooTrapBlock extends HorizontalDirectionalBlock implements Entit
         super.tick(state, level, pos, random);
     }
 
-    protected BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
+    public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
         return !state.canSurvive(level, currentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, facing, facingState, level, currentPos, facingPos);
     }
 
@@ -110,16 +109,10 @@ public class BambooTrapBlock extends HorizontalDirectionalBlock implements Entit
         return !state.getCollisionShape(level, pos).getFaceShape(Direction.UP).isEmpty() || state.isFaceSturdy(level, pos, Direction.UP);
     }
 
-    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
         BlockPos blockpos = pos.below();
         return this.mayPlaceOn(level.getBlockState(blockpos), level, blockpos);
     }
-
-    @Override
-    protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
-        return CODEC;
-    }
-
 
     @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
@@ -127,7 +120,7 @@ public class BambooTrapBlock extends HorizontalDirectionalBlock implements Entit
     }
 
     @Override
-    protected RenderShape getRenderShape(BlockState state) {
+    public RenderShape getRenderShape(BlockState state) {
         return RenderShape.ENTITYBLOCK_ANIMATED;
     }
 
